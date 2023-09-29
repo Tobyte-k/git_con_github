51af67c42760fd76ade0d3f168f83c39196b8100
fernandoperulan <fernandoperulan94@gmail.com>
2023-09-29 17:19:22 -0300

    agregando comidass

---
+++ jampn.java
@@ -0,0 +1,14 @@
+ublic class Main {
+    public static void main(String[] args) {
+
+        // CLASE 2 RE
+
+        String nombre = "Fernando";
+        int edadFer = 29;
+        final double altura = 1.76;
+
+        System.out.println("Hola mi nombre " + nombre + " Edad " + edadFer + " altura " + altura);
+
+
+    }
+}
